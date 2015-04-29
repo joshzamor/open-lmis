226bc076efc31bcddea6c9b193416eeddf2ae431
Chongsun Ahn <cahn922@yahoo.com>
2015-04-29 14:02:08 -0700

    Fix add/edit inventory prototype screen based on recent feedback
    List of changes:
    
    * Replace equipment type at top section with facility
    * Rearrange fields into different sections (non-editable and editable)
    * Reverse dropdown of equipment field and manu/model fields so that user selects manufacturer, then model and equipment field (and energy source) auto-populates
    * Certain fields are non-editable in edit screen that can be set in add screen
    * Stabilizer field values are Yes, No

--- modules/openlmis-web/src/main/webapp/public/js/admin/equipment/inventory/controller/create-equipment-inventory-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/admin/equipment/inventory/controller/create-equipment-inventory-controller.js
@@ -8,29 +8,91 @@
  * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
  */
 
-function CreateEquipmentInventoryController($scope, $location, $routeParams, EquipmentInventory, Donors ,Equipments, SaveEquipmentInventory, Facility, EquipmentOperationalStatus, messageService) {
+function CreateEquipmentInventoryController($scope, $location, $routeParams, EquipmentInventory, Donors ,Equipments, SaveEquipmentInventory, Facility, EquipmentInventoryFacilities, EquipmentOperationalStatus, messageService) {
 
   $scope.$parent.message = $scope.$parent.error = '';
 
   $scope.max_year = new Date().getFullYear();
   $scope.submitted = false;
   $scope.showError = false;
+
   $scope.equipmentTypes = [];
+  $scope.manuModels = [];
+  $scope.models = [];
+  $scope.from = $routeParams.from;
+
   Equipments.get(function (data) {
     $scope.equipments = data.equipments;
     for (var i = 0; i < data.equipments.length; i++) {
       $scope.equipmentTypes[data.equipments[i].equipmentTypeId] = data.equipments[i].equipmentType;
+
+      // Create manu/model object array
+      if (data.equipments[i].equipmentTypeId === 3) { // Only create for CCE
+        var manuModelArray = data.equipments[i].name.split(" / ");
+        var manu = manuModelArray[0];
+        var model = manuModelArray[1];
+        var manuFound = false;
+        for (var j = 0; j < $scope.manuModels.length; j++) {
+          if ($scope.manuModels[j].name === manu) { // Found manu in array, add model to list
+            $scope.manuModels[j].models.push(model);
+            manuFound = true;
+          }
+        }
+        if (!manuFound) {
+          $scope.manuModels.push({name: manu, models: [model]});
+        }
+      }
+
+      // Inject test energy source data into equipments
+      switch (i % 3) {
+        case 0:
+          $scope.equipments[i].energySource = "Gas";
+          break;
+        case 1:
+          $scope.equipments[i].energySource = "Electricity";
+          break;
+        case 2:
+          $scope.equipments[i].energySource = "Solar";
+          break;
+      }
     }
+    console.log($scope.manuModels);
   });
 
-  if ($routeParams.id === undefined) {
-    $scope.equipment = {};
-    $scope.equipment.programId = $routeParams.program;
-    $scope.equipment.facilityId = $routeParams.facility;
-
+  if ($scope.from === "0") { // My facility
     Facility.get({id: $routeParams.facility}, function(data){
       $scope.facility = data.facility;
+      $scope.equipment.facilityId = $scope.facility.id;
+      $scope.facilityDisplayName = $scope.facility.code + " - " + $scope.facility.name;
     });
+  } else { // Supervised facilities
+    $scope.facilities = [
+      {
+        code: "F10",
+        description: "Dispensary",
+        id: 387,
+        name: "Dispensary",
+        virtualFacility: false
+      },
+      {
+        code: "F11",
+        description: "Central Hospital",
+        id: 388,
+        name: "Central Hospital",
+        virtualFacility: false
+      }
+    ];
+    /*      EquipmentInventoryFacilities.get({programId: 5}, function (data) {
+     $scope.vacFacilities = data.facilities;
+     }, {});
+     */
+  }
+
+
+  if ($routeParams.id === undefined) { // Add New
+    $scope.screenType = "create";
+    $scope.equipment = {};
+    $scope.equipment.equipmentTypeId = $routeParams.equipmentType;
 
     // set default of checkboxes so the submission does not become null and hence an error.
     $scope.equipment.replacementRecommended = false;
@@ -38,18 +100,22 @@ function CreateEquipmentInventoryController($scope, $location, $routeParams, Equ
     $scope.equipment.dateLastAssessed = Date.now();
     $scope.equipment.isActive = true;
 
-  } else {
+  } else { // Edit
+    $scope.screenType = "edit";
     EquipmentInventory.get({
       id: $routeParams.id
     }, function (data) {
-      $scope.equipment = angular.copy(data.inventory);
+      $scope.equipment = data.inventory;
+      $scope.equipment.equipmentTypeId = $routeParams.equipmentType;
+
       $scope.equipment.dateLastAssessed = $scope.equipment.dateLastAssessedString ;
       $scope.equipment.dateDecommissioned = $scope.equipment.dateDecommissionedString;
       $scope.equipment.serviceContractEndDate = $scope.equipment.serviceContractEndDateString;
-      $scope.equipment.equipmentTypeId = $scope.equipment.equipment.equipmentTypeId;
-      Facility.get({ id: $scope.equipment.facilityId }, function(data){
+
+      $scope.updateEquipments();
+/*      Facility.get({ id: $scope.equipment.facilityId }, function(data){
         $scope.facility = data.facility;
-      });
+      });*/
     });
   }
 
@@ -61,13 +127,32 @@ function CreateEquipmentInventoryController($scope, $location, $routeParams, Equ
     $scope.donors = data.donors;
   });
 
-  $scope.updateManuModel = function () {
+  $scope.updateModels = function () {
+    $scope.equipment.model = null;
+    $scope.equipment.equipmentId = null;
+    $scope.equipmentDisplayName = "";
+    $scope.equipment.energySource = "";
+    $scope.models = [];
+    var arr = $scope.manuModels;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].name === $scope.equipment.manufacturerName) {
+        $scope.models = arr[i].models;
+        if (arr[i].models.length === 1) {
+          $scope.equipment.model = $scope.models[0];
+          $scope.updateEquipments();
+        }
+        break;
+      }
+    }
+  };
+
+  $scope.updateEquipments = function () {
     var arr = $scope.equipments;
     for (var i = 0; i < arr.length; i++) {
-      if (arr[i].id.toString() === $scope.equipment.equipmentId) {
-        var manuModelArray = arr[i].name.split(" / ");
-        $scope.equipment.manufacturerName = manuModelArray[0];
-        $scope.equipment.model = manuModelArray[1];
+      if (arr[i].name === $scope.equipment.manufacturerName + " / " + $scope.equipment.model) {
+        $scope.equipment.equipmentId = arr[i].id;
+        $scope.equipmentDisplayName = $scope.equipment.manufacturerName + " / " + $scope.equipment.model;
+        $scope.equipment.energySource = arr[i].energySource;
         break;
       }
     }
