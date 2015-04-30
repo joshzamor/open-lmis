869cfeffec1014e599f6c0e6ca0a657276b0522e
Chrispinus Melkiory <chrismelky05@gmail.com>
2015-04-30 13:21:07 +0300

    Modify cc equipment list

--- modules/openlmis-web/src/main/webapp/public/js/admin/equipment/cold-chain-equipments/controller/create-cold-chain-equipment-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/admin/equipment/cold-chain-equipments/controller/create-cold-chain-equipment-controller.js
@@ -13,7 +13,8 @@ function CreateColdChainEquipmentController($scope, $routeParams, $location, Equ
   //Types/Designations
   $scope.equipmentTypes=[{id:1,name:"REFRIGERATOR"},{id:2,name:"FREEZER"},{id:3,name:"FREEZER AND REFRIGERATOR"}];
   $scope.energySources=[{id:1,name:"Solar"},{id:2,name:"Electricity"},{id:3,name:"Gas"}];
-  $scope.pqsStatus=[{id:1,name:"Authorised"},{id:2,name:"Suspended"}];
+  $scope.pqsStatus=[{id:1,name:"Approved"},{id:2,name:"Suspended"}];
+  $scope.donors=[{id:1,name:"CHAI"},{id:2,name:"VR"}];
 
   EquipmentTypes.get(function (data) {
   //  $scope.equipmentTypes_ = data.equipment_type;
@@ -24,9 +25,11 @@ function CreateColdChainEquipmentController($scope, $routeParams, $location, Equ
   if ($routeParams.id === undefined) {
     $scope.equipment = {};
   } else {
-    var equipments=[{id:1,name:"concord/concord1",designation:"Refrigerator",brand:"Concord",model:"Concord1",code:"CR",pqsCode:"domestic",refrigeratorCapacity:20,freezerCapacity:8},{id:2,name:"Biplex/Biplex Rural",designation:"Refrigerator&Freezer",brand:"Biplex",model:"Biplex Rural",code:"CR",pqsCode:"domestic",refrigeratorCapacity:55,freezerCapacity:36}];
-    $scope.equipment={id:1,name:"concord/concord1",type:2,brand:"Concord",model:"Concord1",code:"CR",pqsCode:"domestic",refrigeratorCapacity:20,freezerCapacity:8};
-  }
+    var equipments=[{id:1,name:"concord/concord1",type:2,brand:"Concord",model:"Concord1",code:"CR",pqsCode:"domestic",refrigeratorCapacity:20,freezerCapacity:8},{id:2,name:"Biplex/Biplex Rural",type:1,brand:"Biplex",model:"Biplex Rural",code:"CR",pqsCode:"domestic",refrigeratorCapacity:55,freezerCapacity:36}];
+    var equipment_id=parseFloat($routeParams.id);
+    $scope.equipment =_.findWhere(equipments,{id:equipment_id});
+  
+      }
 
   $scope.saveEquipment = function () {
     if(!$scope.equipmentForm.$invalid){
