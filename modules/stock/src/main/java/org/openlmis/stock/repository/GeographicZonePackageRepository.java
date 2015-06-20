9c913d975fbe64cc44aca7159b4874d888d555d5
Vincent Minde <vincentminde@gmail.com>
2015-06-20 23:17:43 +0300

    Fixed merging errors

--- modules/stock/src/main/java/org/openlmis/stock/repository/GeographicZonePackageRepository.java
+++ modules/stock/src/main/java/org/openlmis/stock/repository/GeographicZonePackageRepository.java
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-package org.openlmis.stock.repository;
-
-import org.openlmis.stock.domain.Diluent;
-import org.openlmis.stock.domain.GeographicZonePackage;
-import org.openlmis.stock.repository.mapper.DiluentMapper;
-import org.openlmis.stock.repository.mapper.GeographicZonePackageMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-/**
- * Created by Morley on 6/14/2015.
- */
-
-@Component
-public class GeographicZonePackageRepository {
-
-    @Autowired
-    private GeographicZonePackageMapper mapper;
-
-    public void update(GeographicZonePackage geographicZonePackage){
-        mapper.update(geographicZonePackage);
-    }
-
-    public void insert(GeographicZonePackage geographicZonePackage){
-        mapper.insert(geographicZonePackage);
-    }
-
-    public List<GeographicZonePackage> getAll(){
-        return mapper.getAll();
-    }
-
-    public GeographicZonePackage getById(Long id) {
-        return mapper.getById(id);
-    }
-}
-=======
 package org.openlmis.stock.repository;
 
 import org.openlmis.stock.domain.Diluent;
@@ -64,4 +25,3 @@ public class GeographicZonePackageRepository extends StockRepository<GeographicZ
         return mapper;
     }
 }
->>>>>>> ab13d65384b55a34d30f8383fc3f8589e803e45d
