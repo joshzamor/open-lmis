0b0e2c7fce7ed0e86fa04247cfda86cd19e7f1ba
Vincent Minde <vincentminde@gmail.com>
2015-06-21 00:03:54 +0300

    Changed migrations

--- modules/migration/src/main/resources/db/migration/new_scripts/V480__insert_vaccines.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V480__insert_vaccines.sql
@@ -1,11 +1,11 @@
-INSERT INTO vaccines (id,expire_warning_period,  type,status) VALUES
-(1, '1', 'vaccine', ''),
-(2, '3', 'vaccine', ''),
-(3, '2', 'vaccine', ''),
-(4, '5', 'vaccine', ''),
-(5, '3', 'diluent', ''),
-(6, '5', 'vaccine', ''),
-(7, '3', 'vaccine', ''),
-(8, '5', 'diluent', ''),
-(9, '4', 'vaccine', ''),
-(10, '3', 'vaccine', '');
+INSERT INTO vaccines (id,name,expire_warning_period,  type,status) VALUES
+(1, 'Measles','1', 'vaccine', ''),
+(2, 'BCG','3', 'vaccine', ''),
+(3, 'Rota','2', 'vaccine', ''),
+(4, 'BCG (Serum)', '5', 'vaccine', ''),
+(5, 'Measles - diluent', '3', 'diluent', ''),
+(6, 'OPV', '5', 'vaccine', ''),
+(7, 'DTP-HepB+Hib', '3', 'vaccine', ''),
+(8, 'BCG - diluent', '5', 'diluent', ''),
+(9, 'TT', '4', 'vaccine', ''),
+(10, 'PCV-13', '3', 'vaccine', '');
