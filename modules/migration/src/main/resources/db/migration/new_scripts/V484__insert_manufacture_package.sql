8a1ccd649c86e4ee991eb0692151b580678cede1
Vincent Minde <vincentminde@gmail.com>
2015-06-21 02:23:16 +0300

    Added test migrations

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V484__insert_manufacture_package.sql
@@ -0,0 +1,2 @@
+INSERT INTO manufacture_package (id,shipment_id,manufacture_date,expire_date,lot_number,number_of_doses,delivery_status,purchasing_order_number,vaccine_packaging_id) VALUES
+(1,'003391113197592828', '2014-01-01','2014-01-01', 'KJHDHDUWD982', 1, 'Good', 'GHDDG891234', 1);
